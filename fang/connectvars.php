f3e928194c6313682ba06b1fa01ac027a07f54b4
jserranos09 <64979937+jserranos09@users.noreply.github.com>
2021-03-04 13:39:36 -0800

    Create connectvars.php

---
+++ fang/connectvars.php
@@ -0,0 +1,7 @@
+<?php
+    // define database connection contants
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'juan');
+    define('DB_PASSWORD', 'juan');
+    define('DB_NAME', 'juan');
+?>
